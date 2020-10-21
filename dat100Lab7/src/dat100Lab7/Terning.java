1eb5a4e8bab1629cba7bbd1afa79ae7a75394c11
Elkjøp Låne-PC <Elkjp Lne-PC@DESKTOP-0QLCH2F>
2020-10-21 17:09:22 +0200

    terning

--- dat100Lab7/src/dat100Lab7/Terning.java
+++ dat100Lab7/src/dat100Lab7/Terning.java
@@ -3,28 +3,21 @@ package dat100Lab7;
 import java.util.Random;
 
 public class Terning {
-	public static void main(String[] args) {
-		int oyne=100;
-		Random terning = new Random();
-		int []tab=new int [oyne];
-		
 
-		for (int j = 0; j < oyne/20; j++) {
-			int i = 0;
-			int tabIndekst=0;
-			while (i < oyne/5) {
-				int n = terning.nextInt(7);
-				if (n > 0) {
-					System.out.print(n + "\t");
-					tab[tabIndekst]=n;
-					tabIndekst++;
-					i++;
-				}
-			}System.out.println();
-		}
-		
-		
+	// aktuell verdi for terningen
+	private int oyne;
+	private Random random=new Random();
 
+	public Terning() {
+		this.oyne=0;
 	}
 
+	public int getOyne() {
+
+		return oyne;
+	}
+
+	public void trill() {
+		oyne=random.nextInt(6)+1;
+	}
 }
